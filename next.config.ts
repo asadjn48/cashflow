5ea5fdf3c36294ade2c606220822cde0c189c3c1
asadjn99 <asad.jn99@gmail.com>
2026-02-08 16:17:23 +0500

    update package json & pwa

--- next.config.ts
+++ next.config.ts
@@ -1,25 +1,18 @@
 /* eslint-disable @typescript-eslint/no-explicit-any */
 import type { NextConfig } from "next";
 
-// 1. Setup PWA 
 // eslint-disable-next-line @typescript-eslint/no-require-imports
 const withPWA = require("@ducanh2912/next-pwa").default({
   dest: "public",
-  disable: process.env.NODE_ENV === "development", 
+  disable: process.env.NODE_ENV === "development",
   register: true,
   skipWaiting: true,
 });
 
-// 2. Define Standard Config
 const nextConfig: NextConfig = {
   
   reactStrictMode: true,
 };
 
 
-export default withPWA({
-  ...nextConfig,
-  eslint: {
-    ignoreDuringBuilds: true,
-  },
-} as any);
+export default withPWA(nextConfig as any);
